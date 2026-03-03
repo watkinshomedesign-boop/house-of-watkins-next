b534f8763820065c08df787086f0ac5c2d7c6f0f
David Watkins <david@houseofwatkins.com>
2026-03-04 01:21:29 +0300

    Add revalidate=60 to /builder page so Sanity CMS changes appear without redeploy

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,6 +1,8 @@
 import { getBuilderLandingPage } from "@/lib/builderPage/sanity";
 import BuilderLandingPage from "./BuilderLandingClient";
 
+export const revalidate = 60;
+
 export default async function BuilderPage() {
   const cms = await getBuilderLandingPage();
   return <BuilderLandingPage cms={cms} />;
